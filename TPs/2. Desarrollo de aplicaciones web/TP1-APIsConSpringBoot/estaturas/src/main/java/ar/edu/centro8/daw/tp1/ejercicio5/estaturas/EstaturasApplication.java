1054fa999495be1c00d984fdd0f64e0c55da4fcd
Pablo Gularte <pablo.gularte@proton.me>
2025-09-30 16:51:02 -0300

    Ejercicio2 TP4 ifce

---
+++ TPs/2. Desarrollo de aplicaciones web/TP1-APIsConSpringBoot/estaturas/src/main/java/ar/edu/centro8/daw/tp1/ejercicio5/estaturas/EstaturasApplication.java
@@ -0,0 +1,13 @@
+package ar.edu.centro8.daw.tp1.ejercicio5.estaturas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EstaturasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EstaturasApplication.class, args);
+	}
+
+}
---
+++ TPs/2. Desarrollo de aplicaciones web/TP1-APIsConSpringBoot/estaturas/src/test/java/ar/edu/centro8/daw/tp1/ejercicio5/estaturas/EstaturasApplicationTests.java
@@ -0,0 +1,13 @@
+package ar.edu.centro8.daw.tp1.ejercicio5.estaturas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EstaturasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
