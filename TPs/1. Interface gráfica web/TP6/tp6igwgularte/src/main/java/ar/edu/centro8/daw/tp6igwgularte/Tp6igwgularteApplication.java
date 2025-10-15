198218970c14d2df16b6b90f9ad1ce9be5c1715a
Pablo Gularte <pablo.gularte@proton.me>
2025-10-15 12:42:26 -0300

    upd estructuta Clases y TPs

---
+++ TPs/1. Interface gráfica web/TP6/tp6igwgularte/src/main/java/ar/edu/centro8/daw/tp6igwgularte/Tp6igwgularteApplication.java
@@ -0,0 +1,13 @@
+package ar.edu.centro8.daw.tp6igwgularte;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Tp6igwgularteApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Tp6igwgularteApplication.class, args);
+	}
+
+}
---
+++ TPs/1. Interface gráfica web/TP6/tp6igwgularte/src/test/java/ar/edu/centro8/daw/tp6igwgularte/Tp6igwgularteApplicationTests.java
@@ -0,0 +1,13 @@
+package ar.edu.centro8.daw.tp6igwgularte;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Tp6igwgularteApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
