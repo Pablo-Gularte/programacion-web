69e7c0108125c9a2c5d19b811531d89200a8fdc8
PabloGularte <pablo.gularte@proton.me>
2025-11-20 18:39:09 -0300

    Cargar menús desplegables y tabla de datos en UI

--- TPs/4. Trabajo Integrador Final/escuelagulartep/src/main/resources/static/js/rutinas.js
+++ TPs/4. Trabajo Integrador Final/escuelagulartep/src/main/resources/static/js/rutinas.js
@@ -1,7 +1,7 @@
 // Al cargar la página recupero los grados para generar los vectores de turnos y grados con los que se completan los menús desplegables
 document.addEventListener("DOMContentLoaded", () => {
     const servidor = "localhost";
-    const puerto = "9090";
+    const puerto = "8080";
     const epGrados = "/grados";
     const urlGrados = `http://${servidor}:${puerto}${epGrados}`;
 
@@ -40,5 +40,81 @@ document.addEventListener("DOMContentLoaded", () => {
 
             // Agrego escuchador de eventos al menu desplegable de grados para que al seleccionar el grado se cargue
             // la tabla de estudiantes filtrando la colección "grados" recuperada del servidor, por grado y turno seleccionados
+            selectGrados.addEventListener("change", () => {
+                const gradoSeleccionadoId = parseInt(selectGrados.value);
+                const turnoSeleccionado = selectTurnos.value;
+                const gradoSeleccionado = grados.find(g => g.id === gradoSeleccionadoId && g.turno === turnoSeleccionado);
+                const contenedorTablaGrados = document.querySelector("#tabla-datos-grados");
+                // Creo el encabezdo de la tabla y una división para separar visualmente
+                const tituloDatosGrados = document.createElement("h2");
+                tituloDatosGrados.className = "pt-4";
+                const division = document.createElement("hr");
+
+                // Limpio la tabla y encabezado de datos del grado
+                contenedorTablaGrados.innerHTML = "";
+                // Si hay grado seleccionado con estudfiantes muestro la tabla, sino muestro mensaje de que no hay estudiantes
+                if (gradoSeleccionado && gradoSeleccionado.estudiantes.length > 0) {
+                    // Creo la tabla con los estudiantes del grado seleccionado
+                    const tabla = document.createElement("table");
+                    tabla.classList.add("table", "table-striped", "table-bordered");
+                    // Creo el encabezado de la tabla
+                    const thead = document.createElement("thead");
+                    const encabezado = document.createElement("tr");
+                    ["ID", "Nombre", "Apellido", "Edad", "Acciones"].forEach(texto => {
+                        const th = document.createElement("th");
+                        th.textContent = texto;
+                        encabezado.appendChild(th);
+                    });
+                    thead.appendChild(encabezado);
+                    tabla.appendChild(thead);
+                    // Creo el cuerpo de la tabla
+                    const tbody = document.createElement("tbody");
+                    gradoSeleccionado.estudiantes.forEach(estudiante => {
+                        const fila = document.createElement("tr");
+                        
+                        // Celda ID, Nombre, Apellido, Edad
+                        const celdaId = document.createElement("td");
+                        celdaId.textContent = estudiante.id;
+                        fila.appendChild(celdaId);
+
+                        const celdaNombre = document.createElement("td");
+                        celdaNombre.textContent = estudiante.nombre;
+                        fila.appendChild(celdaNombre);
+                        
+                        const celdaApellido = document.createElement("td");
+                        celdaApellido.textContent = estudiante.apellido;
+                        fila.appendChild(celdaApellido);
+                        
+                        const celdaEdad = document.createElement("td");
+                        celdaEdad.textContent = estudiante.edad;
+                        fila.appendChild(celdaEdad);
+                        
+                        // Celda de acciones con botones Editar y Eliminar
+                        const celdaAcciones = document.createElement("td");
+                        const botonEditar = document.createElement("button");
+                        botonEditar.textContent = "Editar";
+                        botonEditar.setAttribute('onclick', `editarEstudiante(${estudiante.id})`);
+                        botonEditar.className = "btn btn-primary btn-sm me-2";
+                        celdaAcciones.appendChild(botonEditar);
+                        
+                        const botonEliminar = document.createElement("button");
+                        botonEliminar.textContent = "Eliminar";
+                        botonEliminar.setAttribute('onclick', `eliminarEstudiante(${estudiante.id})`);
+                        botonEliminar.className = "btn btn-danger btn-sm";
+                        celdaAcciones.appendChild(botonEliminar);
+                        fila.appendChild(celdaAcciones);
+                        
+                        // Agrego la fila a la tabla
+                        tbody.appendChild(fila);
+                    });
+                    tabla.appendChild(tbody);
+                    tituloDatosGrados.textContent = `Estudiantes de ${gradoSeleccionado.nombreGrado} (${gradoSeleccionado.turno})`;
+                    contenedorTablaGrados.appendChild(tituloDatosGrados);
+                    contenedorTablaGrados.appendChild(division);
+                    contenedorTablaGrados.appendChild(tabla);
+                } else {
+                    contenedorTablaGrados.textContent = "No hay estudiantes registrados en este grado.";
+                }
+            });
         });
 });
