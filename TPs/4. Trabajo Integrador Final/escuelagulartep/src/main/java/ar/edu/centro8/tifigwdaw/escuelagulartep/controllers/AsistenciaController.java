39f7c189c3179081caed5a16330b79deb17a5ed8
Pablo Gularte <pablo.gularte@proton.me>
2025-12-01 20:33:34 -0300

    Armando controladores para plantillas thymeleaf con fragmentos

--- TPs/4. Trabajo Integrador Final/escuelagulartep/src/main/java/ar/edu/centro8/tifigwdaw/escuelagulartep/controllers/AsistenciaController.java
+++ TPs/4. Trabajo Integrador Final/escuelagulartep/src/main/java/ar/edu/centro8/tifigwdaw/escuelagulartep/controllers/AsistenciaController.java
@@ -4,19 +4,44 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 
+import ar.edu.centro8.tifigwdaw.escuelagulartep.models.Estudiante;
 import ar.edu.centro8.tifigwdaw.escuelagulartep.services.AsistenciaService;
+import ar.edu.centro8.tifigwdaw.escuelagulartep.services.EstudianteService;
 
 @Controller
 public class AsistenciaController {
     private final String urlObtenerTodasLasAsistencias = "/asistencias";
+    private final String urlObtenerAsistenciasPorEstudiante = "/asistencias/{idEstudiante}";
 
-    @Autowired
-    AsistenciaService asistenciaSvc;
+    @Autowired AsistenciaService asistenciaSvc;
+    @Autowired EstudianteService estudianteSvc;
 
     @GetMapping(urlObtenerTodasLasAsistencias)
     public String obtenerTodasLasAsistencias(Model model) {
-        model.addAttribute("asistencias", asistenciaSvc.obtenerTodasLasAsistencias());
-        return "asist";
+
+        // 2. Inyectar los atributos para Thymeleaf
+        model.addAttribute("tituloPagina", "Asistencias de Estudiantes");
+        model.addAttribute("tituloVista", "Listado Completo de Asistencias");
+        model.addAttribute("listaAsistencias", asistenciaSvc.obtenerTodasLasAsistencias());
+        
+        // 3. Especificar el fragmento de contenido a usar
+        model.addAttribute("contenidoFragmento", "asistencia/lista");
+        
+        return "plantilla-listados";
+    }
+    
+    @GetMapping(urlObtenerAsistenciasPorEstudiante)
+    public String obtenerAsistenciasPorEstudiante(Model model, @PathVariable Long idEstudiante) {
+        Estudiante estudiante = estudianteSvc.obtenerEstudiantePorId(idEstudiante);
+
+        model.addAttribute("tituloPagina", "Asistencias de Estudiantes");
+        model.addAttribute("tituloVista", "Listado Completo de Asistencias de " + estudiante.getNombre() + " " + estudiante.getApellido());
+        model.addAttribute("listaAsistencias", asistenciaSvc.obtenerAsistenciasPorEstudiante(idEstudiante));
+        
+        // 3. Especificar el fragmento de contenido a usar
+        model.addAttribute("contenidoFragmento", "asistencia/lista");
+        return "plantilla-listados";
     }
 }
